54832f4d1bf32763ee5c613ab30d3c66fe6e25dc
Pavel Yakovlev <pavel_yakovlev2005@mail.ru>
2025-01-18 10:12:04 +0300

    some little changes

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"RestAPI/config"
 	"RestAPI/routes"
 	"github.com/labstack/echo"
+	"log"
 )
 
 func main() {
 	config.InitDB()
 	e := echo.New()
 	routes.RegisterRoutes(e)
-	e.Start(":8080")
+	if err := e.Start(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
